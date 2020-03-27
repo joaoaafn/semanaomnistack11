e9d2d29feec7bca71fecc937ddb4be092c4fb1ad
Joao <joao.aafn@gmail.com>
2020-03-27 19:03:09 -0300

    application mobile

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -10,6 +10,7 @@ module.exports = {
     console.log(count);
 
     const incidents = await connection('incidents')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
       .limit(5)
       .offset((page - 1) * 5)
       .select('*');
